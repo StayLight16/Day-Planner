bf248fc9483f6d2f7ec5e4fa7334449663e9dda5
Jeremy Yon <jeremyyon87@gmail.com>
2021-01-16 05:04:02 -0600

    more updates

--- script.js
+++ script.js
@@ -171,3 +171,12 @@ function testTime() {
         $(".form5").addClass("present");
     };
 }
+testTime();
+// Loops through input area to get item from local storage
+var x = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+// Test loop:
+for (var i = 0; i < x.length; i++) {
+    var dataHour = localStorage.getItem(x[i]);
+    // form - control
+    $(".form" + x[i]).val(dataHour);
+}
